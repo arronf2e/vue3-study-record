adba335967b807a5510bd7197c79b1bea7d1dc80
zhanghao5 <zhanghao5@yy.com>
2021-04-08 19:12:48 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld :msg="msg" />
+  <button @click="changemsg">change msg</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const msg = ref('Hello Vue 3 + TypeScript + Vite')
+
+    const changemsg = () => msg.value = 'good change'
+
+    return {
+      msg,
+      changemsg
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,91 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <h1>{{ obj.name }}</h1>
+  <h1>{{ count }}</h1>
+  <button @click="countAdd">count++</button>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, onMounted, reactive, onRenderTracked, onRenderTriggered, onErrorCaptured, onUpdated, onBeforeUpdate, onBeforeUnmount, onUnmounted, onActivated, onDeactivated } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+
+  mounted() {
+    console.log('also mounted');
+  },
+
+  setup: (props, ctx) => {
+
+    console.log(props, ctx, props.msg, 'ctx');
+
+    const countAdd = () => count.value++
+
+    onMounted(() => {
+      console.log('hello mounted');
+    })
+
+    onBeforeUpdate((e: any) => {
+      console.log(e, 'before update');
+    })
+
+    onUpdated(() => {
+      console.log('updated');
+    })
+
+    onBeforeUnmount(() => {
+
+    })
+    
+    onUnmounted(() => {
+
+    })
+
+    onActivated(() => {
+      console.log('onActivated');
+      
+    })
+
+    onRenderTracked((event) => {
+      // console.log('hello onrendertracked', event);
+    })
+
+    onRenderTriggered((event) => {
+      console.log('hello onRenderTriggered', event);
+    })
+
+    onErrorCaptured((event) => {
+      console.log(event, 'event');
+    })
+
+    const count = ref(0)
+    const obj = reactive({
+      name: 'obj2'
+    })
+    return { count, obj, countAdd }
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
